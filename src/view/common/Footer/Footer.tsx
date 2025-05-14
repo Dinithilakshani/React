0558ddbf6cf857f454715115e42c95b592631ea0
Dinithilakshani <dinithpalliyaguru@gmail.com>
2025-05-14 14:52:21 +0530

    2

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -1,8 +1,46 @@
- import './Footer.css'
+import './footer.css';
+
  export function Footer() {
-    return (
-        <div className="footer">
-            <h1>This is the Footer</h1>
-        </div>
-    );
-}
+     return (
+         <footer className="footer-container">
+             <div className="footer-content">
+                 <div className="footer-section about">
+                     <div className="footer-logo">
+                         <img src="/images/img.png" alt="Company Logo" />
+                     </div>
+                     <p className="footer-description">
+                         We provide innovative solutions to help businesses grow and succeed in today's digital landscape.
+                     </p>
+                     <div className="footer-social">
+                         <div className="social-icon"><i className="fa fa-facebook"> <img src="/images/img.png"/></i></div>
+                         <div className="social-icon"><i className="fa fa-twitter"></i></div>
+                         <div className="social-icon"><i className="fa fa-instagram"></i></div>
+                         <div className="social-icon"><i className="fa fa-linkedin"></i></div>
+                     </div>
+                 </div>
+
+                 <div className="footer-section links">
+                     <h3>Quick Links</h3>
+                     <ul>
+                         <li><a href="#home">Home</a></li>
+                         <li><a href="#about">About Us</a></li>
+                         <li><a href="#services">Services</a></li>
+                         <li><a href="#portfolio">Portfolio</a></li>
+                         <li><a href="#contact">Contact</a></li>
+                     </ul>
+                 </div>
+
+                 <div className="footer-section contact">
+                     <h3>Contact Us</h3>
+                     <p><i className="fa fa-map-marker"></i> 123 Shilawansha Road Goviyapana Ahangama</p>
+                     <p><i className="fa fa-phone"></i> +94 5678980</p>
+                     <p><i className="fa fa-envelope"></i> Dinithi@gmail.com</p>
+                 </div>
+             </div>
+
+             <div className="footer-bottom">
+                 <p>&copy; {new Date().getFullYear()} Company Name. All Rights Reserved.</p>
+             </div>
+         </footer>
+     );
+ }
--- src/view/common/navBar/NavBar.tsx
+++ src/view/common/navBar/NavBar.tsx
@@ -1,6 +1,17 @@
 import './navbar.css'
 export function NavBar() {
     return (
-        <div className="navbar">This is NavBar</div>
+
+        <nav className="navbar-container">
+            <div className="navbar-logo">
+                <img src="/imges/img.png" alt="Company Logo"/>
+            </div>
+            <div className="navbar-links">
+                <div className="navbar-item active">Home</div>
+                <div className="navbar-item">About</div>
+                <div className="navbar-item">Services</div>
+                <div className="navbar-item">Contact</div>
+            </div>
+        </nav>
     );
 }
